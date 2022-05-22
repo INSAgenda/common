f082782899d8b21ac5da53d1450c8ea4384745ef
Mubelotix <mubelotix@gmail.com>
2022-05-22 22:55:39 +0200

    Add Clone on UserInfo

--- src/user_info.rs
+++ src/user_info.rs
@@ -1,6 +1,6 @@
 use crate::prelude::*;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct UserInfo {
     /// The count of api keys
     pub api_key_count: u64,
