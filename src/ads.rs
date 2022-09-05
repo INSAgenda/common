bbd8a0045064c4e9eb25f196be7c318b49c40729
Mubelotix <mubelotix@gmail.com>
2022-09-05 18:59:13 +0200

    Add content type

--- src/ads.rs
+++ src/ads.rs
@@ -2,13 +2,20 @@ use serde::{Serialize, Deserialize};
 use crate::prelude::*;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum AdContentType {
+    Text,
+    Html,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AdRecord {
     pub title: String,
     pub id: String,
     pub start_ts: u64,
     pub end_ts: u64,
     pub target: Option<GroupDescriptor>,
+    pub ty: AdContentType,
 
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub html: Option<String>,
+    pub content: Option<String>,
 }
