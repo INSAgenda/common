04e18c07a8684a67fdd325b65e69a32aa0685302
Mubelotix <mubelotix@gmail.com>
2022-10-02 14:16:29 +0200

    Move to module

---
+++ src/groups/mod.rs
@@ -0,0 +1,11 @@
+mod group;
+mod group_descriptor;
+mod group_filter;
+mod group_serde;
+mod parsing;
+
+pub use group::*;
+pub use group_descriptor::*;
+pub use group_filter::*;
+pub use group_serde::*;
+pub(crate) use parsing::*;
