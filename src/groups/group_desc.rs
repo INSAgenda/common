52bf6188c77404cdd2c18ef65f4f6d10d653c639
Mubelotix <mubelotix@gmail.com>
2022-10-02 16:26:59 +0200

    Add translations

--- src/groups/group_desc.rs
+++ src/groups/group_desc.rs
@@ -3,9 +3,9 @@ use crate::prelude::*;
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct GroupDesc {
     pub id: String,
-    pub name: String,
-    pub help: String,
-    pub values: Vec<(String, String)>,
+    pub name: (String, String), // (french name, english name)
+    pub help: (String, String), // (french help message, english help message)
+    pub values: Vec<(String, (String, String))>, // (value, (french label, english label))
 
     #[serde(skip_serializing_if = "Option::is_none")]
     pub required_if: Option<GroupFilter>,
