e2ce367d3d3335f45d30dd56e786c702f58194cd
Mubelotix <mubelotix@gmail.com>
2022-10-02 16:49:58 +0200

    Remove useless import

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,3 @@
 pub use crate::{events::*, groups::*, location::*, user_info::*, password::*, announcements::*};
 pub(crate) use serde::{Serialize, Deserialize};
-pub(crate) use std::str::FromStr;
 pub(crate) use std::collections::BTreeMap;
