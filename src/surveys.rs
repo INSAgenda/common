ed2fa141ec437f1ac7b336d23f45bbf685fa0ad9
DimitriTimoz <45998396+DimitriTimoz@users.noreply.github.com>
2022-11-20 18:44:45 +0100

    survey with multiple questions

--- src/surveys.rs
+++ src/surveys.rs
@@ -4,27 +4,12 @@ use serde::{Serialize, Deserialize};
 
 use crate::UserGroups;
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
-pub enum PossibleAnswers {
-    Input(u16),
-    MultipleChoice(HashMap<u16, (String, String)>),
-    OneChoice(HashMap<u16, (String, String)>),
-    Priority(HashMap<u16, (String, String)>),
-    Value { min: f64, max: f64, step: f64 },
-}
-
-impl Default for PossibleAnswers {
-    fn default() -> Self {
-        Self::Input(12)
-    }
-}
-
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub enum Answer {
     Input(String),
     MultipleChoice(Vec<u16>),
     OneChoice(u16),
-    Priority(Vec<(u16, u16)>),
+    Priority(HashMap<u16, u16>),
     Value(f64),
 }
 
@@ -38,30 +23,55 @@ impl Default for Answer {
 #[derive(Serialize, Clone, Debug, PartialEq)]
 pub struct Survey {
     pub id: String,
-    pub question: (String, String),
-    pub answers: PossibleAnswers,
+    pub description: (String, String),
+    pub questions: HashMap<u16, Question>,
     pub date_start: i64,
     pub date_end: i64,
-    pub can_edit: bool,
     pub target: UserGroups,
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+pub enum PossibleAnswer {
+    Input(u16),
+    MultipleChoice(HashMap<u16, (String, String)>),
+    OneChoice(HashMap<u16, (String, String)>),
+    Priority(HashMap<u16, (String, String)>),
+    Value { min: f64, max: f64, step: f64 },
+}
+
+impl Default for PossibleAnswer {
+    fn default() -> Self {
+        Self::Input(12)
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+pub struct Question {
+    pub question: (String, String),
+    pub possible_answer: PossibleAnswer,
+    pub can_edit: bool,
+}
+
 #[derive(Serialize, Clone, Debug, PartialEq)]
-pub struct SurveyAnswer {
+pub struct SurveyAnswers {
     pub id: String,
-    pub answer: Answer,
+    pub answers: HashMap<u16, Answer>,
     pub last_mod: i64,
 }
 
 impl Survey {
     pub fn new(id: String) -> Self {
+        let mut questions = HashMap::new();
+        questions.insert(0, Question {
+            question: (String::from("Question"), String::from("Question")),
+            possible_answer: PossibleAnswer::Input(12),
+            can_edit: true,
+        });
+
         Self {
             id,
-            question: (
-                "What is your favorite color?".to_string(),
-                "Quelle est votre couleur préférée?".to_string(),
-            ),
-            
+            description: (String::from("Description"), String::from("Description")),
+            questions,
             date_start: std::time::SystemTime::now()
                 .duration_since(std::time::UNIX_EPOCH)
                 .unwrap()
@@ -71,12 +81,9 @@ impl Survey {
                 .unwrap()
                 .as_secs() as i64
                 + 10000000000,
-            can_edit: false,
-            answers: PossibleAnswers::default(),
             target: UserGroups::new_with_groups(BTreeMap::from([
                 ("school".to_string(), "insa-rouen".to_string()),
             ])),
-            
         }
     }
 }
