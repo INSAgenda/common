73382070d93882b76be46407417d6690ad4df406
DimitriTimoz <dimitri.timoz@orange.fr>
2023-01-18 15:21:47 +0100

    UserDesc

---
+++ src/user_desc.rs
@@ -0,0 +1,23 @@
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UserDesc {
+    pub uid: i64,
+    pub email: String,
+    pub profile_picture: Option<String>,
+}
+
+impl UserDesc {
+    /// Creates a new UserDesc.
+    pub fn new(uid: i64, email: String) -> Self {
+        UserDesc {
+            uid,
+            email,
+        }
+    }
+
+    /// Returns the username of the user according to the email address.
+    /// example: "edouard.foobar@insa-rouen.fr -> "edouard.foobar"
+    pub fn get_username(&self) -> String {
+        self.email.split("@").next().unwrap().to_string()
+    }
+}
