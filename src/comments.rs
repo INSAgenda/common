1b4ce2b29d833c6ea22a196a447536e0691f23be
Mubelotix <mubelotix@gmail.com>
2023-01-26 14:35:41 +0100

    Add Comment struct

---
+++ src/comments.rs
@@ -0,0 +1,23 @@
+use crate::prelude::*;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Comment {
+    /// Random number identifying the comment.
+    pub id: u64,
+    /// Id of the parent comment, if any.
+    pub parent: Option<u64>,
+    /// Author of the comment.
+    pub author: UserDesc,
+    /// Content of the comment.
+    /// Markdown isn't supported but will be eventually.
+    pub content: String,
+    /// Timestamp of the comment creation.
+    pub creation_ts: i64,
+    /// Equal to `creation_ts` if the comment has never been edited.
+    pub last_edited_ts: i64,
+    /// Number of upvotes minus number of downvotes.
+    pub votes: i64,
+    /// The vote of the current user.
+    /// -1, 0 or 1.
+    pub vote: u8,
+}
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,4 @@
-pub use crate::{events::*, groups::*, location::*, user_info::*, password::*, announcements::*, surveys::*, friends::*, user_desc::*};
+pub use crate::{events::*, groups::*, location::*, user_info::*, password::*, announcements::*, surveys::*, friends::*, user_desc::*, comments::*};
 pub(crate) use serde::{Serialize, Deserialize};
 pub(crate) use std::collections::BTreeMap;
 pub(crate) use std::collections::HashMap;
